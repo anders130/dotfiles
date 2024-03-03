b2ea4c480e772408163ba8acfc73f420f91b64e3
anders130 <jesse.gollub@ewe.net>
2024-03-03 13:45:59 +0100

    added fastfetch greeting

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
+function fish_greeting
+    fastfetch -c ~/.dotfiles/fastfetch/shell-greeting.jsonc
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_vi_key_bindings
