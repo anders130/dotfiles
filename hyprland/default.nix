a693930d3eb0fa30852052028be53858af0556cb
anders130 <jesse.gollub@ewe.net>
2024-03-27 11:20:00 +0100

    modularized hyprland config

--- hyprland/default.nix
+++ hyprland/default.nix
@@ -12,6 +12,9 @@
     services.xserver.displayManager.defaultSession = "hyprland";
 
     home-manager.users.${username} = { config, ... }: {
-        xdg.configFile."hypr/hyprland.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/hyprland/hyprland.conf";
+        xdg.configFile.hypr = {
+            source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/hyprland";
+            recursive = true;
+        };
     };
 }
