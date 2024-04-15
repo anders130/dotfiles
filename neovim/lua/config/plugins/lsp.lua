b85f30f958ccf597ced173cf5c80b1ea4ef274b9
anders130 <jesse.gollub@ewe.net>
2024-04-15 22:01:07 +0200

    neovim: updated plugins

--- neovim/lua/config/plugins/lsp.lua
+++ neovim/lua/config/plugins/lsp.lua
@@ -113,7 +113,8 @@ return {
             --    https://github.com/pmizio/typescript-tools.nvim
             --
             -- But for many setups, the LSP (`tsserver`) will work just fine
-            -- tsserver = {},
+            tsserver = {},
+            cssls = {},
             --
 
             lua_ls = {
--- neovim/lua/config/plugins/misc.lua
+++ neovim/lua/config/plugins/misc.lua
@@ -1,6 +1,6 @@
 return {
     -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-    'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+    -- 'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
     -- "gc" to comment visual regions/lines
     { 'numToStr/Comment.nvim', opts = {} },
