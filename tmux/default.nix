f1410a8f63828c845285eca8bcdaa75a67850f98
anders130 <jesse.gollub@ewe.net>
2024-05-06 13:23:42 +0200

    updated tmux config

--- tmux/default.nix
+++ tmux/default.nix
@@ -10,13 +10,16 @@
         # Force tmux to use /tmp for sockets (WSL2 compat)
         secureSocket = false;
         clock24 = true;
+        keyMode = "vi";
+        shortcut = "Space"; # Ctrl+Space
+        baseIndex = 1; # window and pane index
+        newSession = true; # if failing tmux a, create new session
 
-        plugins = with pkgs.tmuxPlugins; [
-            sensible
+        plugins = with pkgs.unstable.tmuxPlugins; [
+            # sensible
             vim-tmux-navigator
             catppuccin
-            continuum
-            yank
+            # yank
         ];
 
         extraConfigBeforePlugins = ''
