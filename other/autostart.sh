84a50cde429ce7d6aaa73b0e6fba3f266666f8fa
anders130 <jesse.gollub@ewe.net>
2024-05-08 13:31:31 +0200

    added autostart script

---
+++ other/autostart.sh
@@ -0,0 +1,6 @@
+# only execute if noisetorch is not yet active (grep finds <= 1 results)
+if [ "$(wpctl status | grep -c 'NoiseTorch Microphone')" -le 1 ]; then
+    sleep 1 && noisetorch -i
+fi
+
+pkill tmux
