d06897ca44f55fed743e804187acf1d317bb4998
anders130 <jesse.gollub@ewe.net>
2024-05-22 11:52:16 +0200

    installed nvidia drive version 555.42.02

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,6 +1,6 @@
 {
-    pkgs,
     config,
+    lib,
     ...
 }: let
     mountOptions = [
@@ -47,23 +47,15 @@ in {
         powerManagement.enable = false;
         open = false;
         nvidiaSettings = true;
-
-        package = let 
-            rcu_patch = pkgs.fetchpatch {
-                url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
-                hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
-            };
-        in config.boot.kernelPackages.nvidiaPackages.mkDriver {
-            version = "535.154.05";
-            sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
-            sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
-            openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
-            settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
-            persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
-
-            patches = [ rcu_patch ];
+        package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+            version = "555.42.02";
+            sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
+            sha256_aarch64 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+            openSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+            settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA="; 
+            persistencedSha256 = lib.fakeSha256;
         };
-    }; 
+    };
     hardware.opengl = {
         enable = true;
         driSupport = true;
