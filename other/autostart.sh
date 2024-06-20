a263c955ab9e01e5861bc6f7205128e4ac452eb9
anders130 <93037023+anders130@users.noreply.github.com>
2024-06-20 07:45:37 +0200

    fixed hyprsome workspace creation with hacky autostart movement

--- other/autostart.sh
+++ other/autostart.sh
@@ -1,3 +1,9 @@
+# fix hyprsome monitor workspaces
+hyprctl dispatch movefocus r
+hyprctl dispatch movefocus r
+hyprsome workspace 1
+hyprctl dispatch movefocus l
+
 # only execute if noisetorch is not yet active (grep finds <= 1 results)
 if [ "$(wpctl status | grep -c 'NoiseTorch Microphone')" -le 1 ]; then
     sleep 1 && noisetorch -i
