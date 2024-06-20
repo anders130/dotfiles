a0709cc500c7065f1ed9a8a6034efad6b7b07d48
anders130 <93037023+anders130@users.noreply.github.com>
2024-06-20 12:57:06 +0200

    added documents to home directory

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -5,6 +5,7 @@
     symlink = config.lib.file.mkOutOfStoreSymlink;
 in{
     home.file = {
+        Documents.source = symlink "/mnt/data/Documents";
         Downloads.source = symlink "/mnt/data/Downloads";
         Music.source = symlink "/mnt/data/Music";
         Pictures.source = symlink "/mnt/data/Pictures";
