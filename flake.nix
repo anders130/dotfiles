c45beffd7fe66c265e263bd3a16736782194dd43
anders130 <93037023+anders130@users.noreply.github.com>
2024-07-31 19:24:28 +0200

    fixed hyprland version pinning

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
 
         # gui
         ags.url = "github:Aylur/ags";
-        hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+        hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&rev=918d8340afd652b011b937d29d5eea0be08467f5";
 
         split-monitor-workspaces = {
             url = "github:Duckonaut/split-monitor-workspaces";
