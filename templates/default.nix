2d1c0d0ca411bf5d9aaef5acc102034f5b076819
anders130 <93037023+anders130@users.noreply.github.com>
2024-09-08 17:18:44 +0200

    updated templates
    this messed up nix flake check

--- templates/default.nix
+++ templates/default.nix
@@ -7,15 +7,12 @@
         '';
     };
 
-    dev = {
-        description = "dev environment templates";
-        simple = {
-            path = ./dev/simple;
-            description = "simple dev environment with no pre-installed packages";
-            welcomeText = ''
-                This template is a basic dev environment with no pre-installed packages.
-                Add your own packages to the devShells.default package list.
-            '';
-        };
+    simple = {
+        path = ./dev/simple;
+        description = "simple dev environment with no pre-installed packages";
+        welcomeText = ''
+            This template is a basic dev environment with no pre-installed packages.
+            Add your own packages to the devShells.default package list.
+        '';
     };
 }
