52e190b0a7e7b020af57427c6b6202a6b170394e
anders130 <93037023+anders130@users.noreply.github.com>
2024-09-13 17:47:17 +0200

    added samba service

--- hosts/nebulon/default.nix
+++ hosts/nebulon/default.nix
@@ -12,6 +12,37 @@
         plex.enable = true;
     };
 
+    services.samba = {
+        enable = true;
+        securityType = "user";
+        openFirewall = true;
+        extraConfig = ''
+            workgroup = WORKGROUP
+            server string = smbnix
+            netbios name = smbnix
+            security = user
+            #use sendfile = yes
+            #max protocol = smb2
+            # note: localhost is the ipv6 localhost ::1
+            hosts allow = 192.168.178. 127.0.0.1 localhost
+            hosts deny = 0.0.0.0/0
+            guest account = nobody
+            map to guest = bad user
+        '';
+        shares = {
+            root = {
+                path = "/";
+                browseable = "yes";
+                "read only" = "no";
+                "guest ok" = "yes";
+                "create mask" = "0644";
+                "directory mask" = "0755";
+                "force user" = "admin";
+                "force group" = "users";
+            };
+        };
+    };
+
     # Bootloader.
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
