6a8529e53e38b9120d3cd5c63cdf834904a23d98
anders130 <93037023+anders130@users.noreply.github.com>
2024-09-13 12:56:24 +0200

    added lumehub to nix-pi

--- hosts/nix-pi/default.nix
+++ hosts/nix-pi/default.nix
@@ -3,21 +3,17 @@
 
     raspberry-pi-nix.board = "bcm2711";
 
-    hardware = {
-        bluetooth.enable = true;
-        raspberry-pi = {
-            config = {
-                all = {
-                    base-dt-params = {
-                        # enable autoprobing of bluetooth driver
-                        # https://github.com/raspberrypi/linux/blob/c8c99191e1419062ac8b668956d19e788865912a/arch/arm/boot/dts/overlays/README#L222-L224
-                        krnbt = {
-                            enable = true;
-                            value = "on";
-                        };
-                    };
-                };
-            };
+    hardware.raspberry-pi.config.all.base-dt-params.spi = {
+        enable = true;
+        value = "on";
+    };
+
+    services.lumehub = {
+        enable = true;
+        openFirewall = true;
+        settings.LedControllerSettings = {
+            PixelCount = 256;
+            ClockFrequency = 1500000;
         };
     };
 }
