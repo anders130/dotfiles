4ce5e9e12d118d185a0672fa22338b756fffd827
anders130 <93037023+anders130@users.noreply.github.com>
2024-09-14 15:44:19 +0200

    ags: updated bar styling

--- modules/ags/components/Bar/components/hyprland.js
+++ modules/ags/components/Bar/components/hyprland.js
@@ -1,68 +1,84 @@
-import { Application } from "resource:///com/github/Aylur/ags/service/applications.js"
-import { getIcons } from "../../../utils/icons.js"
+import { Application } from 'resource:///com/github/Aylur/ags/service/applications.js'
+import { getIcons } from '../../../utils/icons.js'
 
-const hyprland = await Service.import("hyprland")
+const hyprland = await Service.import('hyprland')
 
 export const Workspaces = ({ monitor = 0 }) => {
-    const activeId = hyprland.active.workspace.bind("id")
-    const workspaces = hyprland.bind("workspaces").as(ws => ws
-        .sort((a, b) => Math.abs(a.id) - Math.abs(b.id))
-        .filter(({ id }) => {
-            if (id < 0) return true;
-            const workspaceMonitor = Math.floor((id - 1) / 10)
-            return workspaceMonitor === monitor
-        })
-        .map(({ id }) => {
-            const adjustedId = id < 0
-                ? id
-                : id % 10
-            return Widget.Button({
-                on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
-                child: Widget.Box({
-                    children: [
-                        Widget.Label(`${adjustedId}`),
-                        Clients(id)
-                    ],
-                    spacing: 16
-                }),
-                class_name: activeId.as(i => `${i === id ? "focused" : ""}`),
+    const workspaces = hyprland.bind('workspaces').as((ws) =>
+        ws
+            .sort((a, b) => Math.abs(a.id) - Math.abs(b.id))
+            .filter(({ id }) => {
+                if (id < 0) return true
+                const workspaceMonitor = Math.floor((id - 1) / 10)
+                return workspaceMonitor === monitor
             })
-        })
+            .map(({ id }) => {
+                const adjustedId = id < 0 ? id : id % 10
+                return Widget.Button({
+                    on_clicked: () =>
+                        hyprland.messageAsync(`dispatch workspace ${id}`),
+                    child: Widget.Box({
+                        children: [Widget.Label(`${adjustedId}`), Clients(id)],
+                        spacing: 16
+                    }),
+                    class_name: hyprland.bind('monitors').as((monitors) => {
+                        const isActive = monitors
+                            .map((m) => m.activeWorkspace)
+                            .map((w) => w.id)
+                            .includes(id)
+                        const isFocused = monitors
+                            .filter((m) => m.focused)
+                            .map((m) => m.id)
+                            .includes(monitor)
+                        return !isActive ? '' : isFocused ? 'focused' : 'active'
+                    })
+                })
+            })
     )
 
     return Widget.Box({
-        class_name: "workspaces",
-        children: workspaces,
+        class_name: 'workspaces',
+        children: workspaces
     })
 }
 
 /** @param {Application | undefined} app */
-const AppIcon = app => Widget.Icon({
-    icon: app?.icon_name || "",
-    size: 16,
-    class_name: "app-icon"
-})
+const AppIcon = (app) =>
+    Widget.Icon({
+        icon: app?.icon_name || '',
+        size: 16,
+        class_name: 'app-icon'
+    })
 
 /** @param {number} workspaceId */
-export const Clients = workspaceId => {
-    const clients = hyprland.bind("clients").as(cs => cs
-        .filter(c => c.workspace.id == workspaceId)
-        .filter(c => c.class !== "steam" || c.title === "Steam")
-        .filter(c => (c.class !== "Google-chrome" || c.initialTitle.includes("Chrome")
-            || c.initialTitle === "Microsoft Teams"
-            || c.initialTitle === "YouTube Music")
-            && c.initialTitle.trim() !== "")
-        .map(c => Widget.Box({
-            children: [AppIcon(getIcons(c))],
-        })))
+export const Clients = (workspaceId) => {
+    const clients = hyprland.bind('clients').as((cs) =>
+        cs
+            .filter((c) => c.workspace.id == workspaceId)
+            .filter((c) => c.class !== 'steam' || c.title === 'Steam')
+            .filter(
+                (c) =>
+                    (c.class !== 'Google-chrome' ||
+                        c.initialTitle.includes('Chrome') ||
+                        c.initialTitle === 'Microsoft Teams' ||
+                        c.initialTitle === 'YouTube Music') &&
+                    c.initialTitle.trim() !== ''
+            )
+            .map((c) =>
+                Widget.Box({
+                    children: [AppIcon(getIcons(c))]
+                })
+            )
+    )
     return Widget.Box({
-        class_name: "clients",
+        class_name: 'clients',
         children: clients,
         spacing: 8
     })
 }
 
-export const ClientTitle = () => Widget.Label({
-    class_name: "client-title",
-    label: hyprland.active.client.bind("title"),
-})
+export const ClientTitle = () =>
+    Widget.Label({
+        class_name: 'client-title',
+        label: hyprland.active.client.bind('title')
+    })
--- modules/ags/components/Bar/index.js
+++ modules/ags/components/Bar/index.js
@@ -1,30 +1,34 @@
-export const Bar = ({
-    monitor = 0,
-    left,
-    center,
-    right
-}) => Widget.Window({
-    name: `bar-${monitor}`,
-    class_name: "bar",
-    monitor,
-    anchor: ["top", "left", "right"],
-    exclusivity: "exclusive",
-    child: Widget.CenterBox({
-        start_widget: Widget.Box({
-            spacing: 8,
-            hpack: "start",
-            children: left,
-        }),
-        center_widget: Widget.Box({
-            spacing: 8,
-            children: center,
-        }),
-        end_widget: Widget.Box({
-            spacing: 8,
-            hpack: "end",
-            children: right,
-        }),
-    }),
-})
+const hyprland = await Service.import('hyprland')
+
+export const Bar = ({ monitor = 0, left, center, right }) => {
+    const activeMonitorId = hyprland.active.monitor.bind('id')
+
+    return Widget.Window({
+        name: `bar-${monitor}`,
+        class_name: activeMonitorId.as(
+            (id) => `bar ${id === monitor ? 'active' : ''}`
+        ),
+        monitor,
+        anchor: ['top', 'left', 'right'],
+        exclusivity: 'exclusive',
+        margins: [5, 5, 0, 5],
+        child: Widget.CenterBox({
+            start_widget: Widget.Box({
+                spacing: 8,
+                hpack: 'start',
+                children: left
+            }),
+            center_widget: Widget.Box({
+                spacing: 8,
+                children: center
+            }),
+            end_widget: Widget.Box({
+                spacing: 8,
+                hpack: 'end',
+                children: right
+            })
+        })
+    })
+}
 
 App.applyCss(`${App.configDir}/components/Bar/style.css`)
