1b5513e6e13e62c6774ecb3e2a947a4b40ee6da4
anders130 <93037023+anders130@users.noreply.github.com>
2024-09-19 13:37:31 +0200

    updated displaylink module to work with latest kernel

--- modules/hardware/displaylink/default.nix
+++ modules/hardware/displaylink/default.nix
@@ -1,6 +1,7 @@
 {
     config,
     lib,
+    pkgs,
     ...
 }: {
     options.modules.hardware.displaylink = {
@@ -9,5 +10,13 @@
 
     config = lib.mkIf config.modules.hardware.displaylink.enable {
         services.xserver.videoDrivers = ["displaylink" "modesetting"];
+
+        boot.extraModulePackages = with config.boot.kernelPackages; [
+            evdi
+        ];
+
+        services.xserver.displayManager.sessionCommands = /*bash*/''
+            ${lib.getBin pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource 2 0
+        '';
     };
 }
--- overlays.nix
+++ overlays.nix
@@ -19,9 +19,29 @@ inputs: rec {
         };
     };
 
+    evdiFix = final: prev: {
+        linuxPackages_latest =
+            prev.linuxPackages_latest.extend
+            (lpfinal: lpprev: {
+                evdi = lpprev.evdi.overrideAttrs (efinal: eprev: {
+                    version = "1.14.6-git";
+                    src = prev.fetchFromGitHub {
+                        owner = "DisplayLink";
+                        repo = "evdi";
+                        rev = "b0fdc340791d01e12796f2df6b62cde576ca4177";
+                        sha256 = "sha256-/XIWacrsB7qBqlLUwIGuDdahvt2dAwiK7dauFaYh7lU=";
+                    };
+                });
+            });
+        displaylink = prev.displaylink.override {
+            inherit (final.linuxPackages_latest) evdi;
+        };
+    };
+
     default = inputs.nixpkgs.lib.composeManyExtensions [
         allowMissingOverlay
         all-channels
         inputs.nix-minecraft.overlay
+        evdiFix
     ];
 }
