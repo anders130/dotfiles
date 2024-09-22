96e6040a6277e0f43270548b6596d6f47c1029f3
anders130 <93037023+anders130@users.noreply.github.com>
2024-09-22 18:59:53 +0200

    ags: updated battery style

--- modules/ags/components/Bar/components/battery.js
+++ modules/ags/components/Bar/components/battery.js
@@ -1,10 +1,19 @@
 const battery = await Service.import('battery')
 
-export const Battery = Widget.CircularProgress({
-    child: Widget.Icon({
-        icon: battery.bind('icon_name')
-    }),
-    visible: battery.bind('available'),
-    value: battery.bind('percent').as((p) => (p > 0 ? p / 100 : 0)),
-    class_name: battery.bind('charging').as((ch) => (ch ? 'charging' : ''))
+const Indicator = Widget.Icon({
+    icon: battery.bind('icon_name')
+})
+
+const LevelBar = Widget.LevelBar({
+    barMode: 'discrete',
+    maxValue: 7,
+    value: battery.bind('percent').as((p) => (p / 100) * 7),
+    margin: 10
+})
+
+export const Battery = Widget.Box({
+    class_name: 'battery-bar',
+    children: [Indicator, LevelBar],
+    tooltipText: battery.bind('percent').as((p) => `${p}%`),
+    visible: battery.bind('available')
 })
