0bcb2b625013ad168b0a3369c374c6317488731d
anders130 <93037023+anders130@users.noreply.github.com>
2024-10-12 14:28:48 +0200

    prevented kanata from screwing with glove80 config

--- modules/hardware/kanata/default.nix
+++ modules/hardware/kanata/default.nix
@@ -20,6 +20,14 @@
                     esc y z
                 )
             '';
+            # reset glove80 configuration
+            keyboards.glove80 = {
+                devices = ["/dev/input/by-id/usb-MoErgo_Glove80_Left_moergo.com:GLV80-BD7B4D3B999EE829-event-kbd"];
+                config = ''
+                    (defsrc)
+                    (deflayer default)
+                '';
+            };
         };
     };
 }
