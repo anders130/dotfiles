ff72c44ee2818e8527e1a0f0b87c86ee5f5555b9
anders130 <93037023+anders130@users.noreply.github.com>
2024-10-12 21:13:14 +0200

    nixvim: modified treesitter config

--- modules/nixvim/config/plugins/treesitter.nix
+++ modules/nixvim/config/plugins/treesitter.nix
@@ -4,27 +4,12 @@
         pattern.".*/hypr/.*%.conf" = "hyprlang";
     };
 
-    extraConfigLua = /*lua*/''
-        require("nvim-treesitter.install").prefer_git = true
-    '';
-
     plugins.treesitter = {
         enable = true;
         settings = {
-            auto_install = false;
-            ensure_installed = "all";
-            prefer_git = true;
-            highlight = {
-                enable = true;
-                # Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-                # If you are experiencing weird indenting issues, add the language to
-                # the list of additional_vim_regex_highlighting and disabled languages for indent.
-                additional_vim_regex_highlighting = [ "ruby" ];
-            };
-            indent = {
-                enable = true;
-                disable = [ "ruby" ];
-            };
+            highlight.enable = true;
+            indent.enable = true;
         };
+        nixvimInjections = true;
     };
 }
