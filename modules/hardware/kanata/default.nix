f2956333bb2e17bd25b173d563a9320674bfe87a
anders130 <93037023+anders130@users.noreply.github.com>
2024-10-13 14:46:09 +0200

    removed y to z remapping and disable glove80 config reset

--- modules/hardware/kanata/default.nix
+++ modules/hardware/kanata/default.nix
@@ -14,20 +14,20 @@
             package = pkgs.kanata;
             keyboards.default.config = ''
                 (defsrc
-                    caps z y
+                    caps
                 )
                 (deflayer default
-                    esc y z
+                    esc
                 )
             '';
             # reset glove80 configuration
-            keyboards.glove80 = {
-                devices = ["/dev/input/by-id/usb-MoErgo_Glove80_Left_moergo.com:GLV80-BD7B4D3B999EE829-event-kbd"];
-                config = ''
-                    (defsrc)
-                    (deflayer default)
-                '';
-            };
+            # keyboards.glove80 = {
+            #     devices = ["/dev/input/by-id/usb-MoErgo_Glove80_Left_moergo.com:GLV80-BD7B4D3B999EE829-event-kbd"];
+            #     config = ''
+            #         (defsrc)
+            #         (deflayer default)
+            #     '';
+            # };
         };
     };
 }
