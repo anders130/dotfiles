b5e96a252c7614ae581f5b27a62b8584bcf1da28
anders130 <93037023+anders130@users.noreply.github.com>
2024-11-06 18:24:50 +0100

    simplified lib

--- lib/default.nix
+++ lib/default.nix
@@ -1,15 +1,10 @@
-{
-    inputs,
-    lib,
-    system,
-    isThinClient,
-}: let
+args: let
     inherit (builtins) readDir filter map attrNames listToAttrs replaceStrings;
 
     dir = filter (name: name != "default.nix") (attrNames (readDir ./.));
     stripNixSuffix = name: replaceStrings [".nix"] [""] name;
 
-    mkImport = path: import path { inherit inputs lib system isThinClient; };
+    mkImport = path: import path args;
 
 in listToAttrs (map (name: {
     name = stripNixSuffix name;
