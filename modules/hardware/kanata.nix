3b1ba7ae4a107b8dfd38bb4191e867869d9a1dec
anders130 <93037023+anders130@users.noreply.github.com>
2024-11-12 15:23:15 +0100

    added german umlauts

--- modules/hardware/kanata.nix
+++ modules/hardware/kanata.nix
@@ -10,18 +10,40 @@
         keyboards.default.config = ''
             (defsrc
                 caps
+                lalt ralt
+                lsft rsft
+                a o u s e
             )
             (deflayer default
                 esc
+                @alt @alt
+                lsft rsft
+                a o u s e
+            )
+            (deflayer unicodechars
+                esc
+                lalt ralt
+                lsft rsft
+                @ae @oe @ue @sz @eu
+            )
+            (defalias
+                _ae (unicode ä)
+                _Ae (unicode Ä)
+                _oe (unicode ö)
+                _Oe (unicode Ö)
+                _ue (unicode ü)
+                _Ue (unicode Ü)
+                _sz (unicode ß)
+                _Sz (unicode ẞ)
+
+                ae (fork @_ae @_Ae (lsft rsft))
+                oe (fork @_oe @_Oe (lsft rsft))
+                ue (fork @_ue @_Ue (lsft rsft))
+                sz (fork @_sz @_Sz (lsft rsft))
+                eu (unicode €)
+
+                alt (tap-hold 150 150 alt (layer-while-held unicodechars))
             )
         '';
-        # reset glove80 configuration
-        # keyboards.glove80 = {
-        #     devices = ["/dev/input/by-id/usb-MoErgo_Glove80_Left_moergo.com:GLV80-BD7B4D3B999EE829-event-kbd"];
-        #     config = ''
-        #         (defsrc)
-        #         (deflayer default)
-        #     '';
-        # };
     };
 }
