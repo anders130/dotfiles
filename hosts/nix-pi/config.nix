a3598105ea682746424c3ac530afc1cea70292fb
anders130 <93037023+anders130@users.noreply.github.com>
2024-11-19 13:57:39 +0100

    fixed raspberry-pi deps

--- hosts/nix-pi/config.nix
+++ hosts/nix-pi/config.nix
@@ -4,7 +4,8 @@ inputs: {
     hashedPassword = "$6$35ci2VTHBQagt2F.$D3rDPc2vNQfZwtbm.T5KIXv6CHy//.0aPLYodHuE7TIUV3yaiwXyNgCZ5/vldVhgqvndaUtiu9XAA2jNyglFT1";
     system = "aarch64-linux";
     isThinClient = true;
-    modules = [
-        inputs.raspberry-pi-nix.nixosModules.raspberry-pi
+    modules = with inputs.raspberry-pi-nix.nixosModules; [
+        raspberry-pi
+        sd-image
     ];
 }
