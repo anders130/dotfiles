fdb63857884035ac65a4e5b3d456f3e78b572733
anders130 <93037023+anders130@users.noreply.github.com>
2024-11-26 20:44:34 +0100

    refactored lib

--- lib/default.nix
+++ lib/default.nix
@@ -1,13 +1,11 @@
 args: let
-    inherit (builtins) readDir filter map attrNames listToAttrs replaceStrings;
-
-    dir = filter (name: name != "default.nix") (attrNames (readDir ./.));
-    stripNixSuffix = name: replaceStrings [".nix"] [""] name;
-
-    mkImport = path: import path args;
-
-in listToAttrs (map (name: {
-    name = stripNixSuffix name;
-    value = mkImport ./${name};
-}) dir)
-
+    inherit (builtins) attrNames filter listToAttrs map readDir replaceStrings;
+in ./.
+    |> readDir
+    |> attrNames # only get the names of the files
+    |> filter (name: name != "default.nix")
+    |> map (name: {
+        name = replaceStrings [".nix"] [""] name;
+        value = import ./${name} args;
+    })
+    |> listToAttrs
