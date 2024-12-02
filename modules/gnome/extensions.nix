b6799ab069123ef3afeb010c5aa1eec5e386653e
anders130 <93037023+anders130@users.noreply.github.com>
2024-12-02 10:05:40 +0100

    blurred windows in gnome

--- modules/gnome/extensions.nix
+++ modules/gnome/extensions.nix
@@ -20,12 +20,19 @@ in {
 
         home-manager.users.${username}.dconf.settings = {
             "org/gnome/shell" = {
-                enabled-extensions = builtins.map (extension: extension.extensionUuid) extensions;
+                enabled-extensions = map (e: e.extensionUuid) extensions;
                 disable-user-extensions = false;
             };
 
             "org/gnome/shell/extensions/blur-my-shell" = {
                 brightness = 0.9;
+            };
+            "org/gnome/shell/extensions/blur-my-shell/applications" = {
+                blue = true;
+                dynamic-opacity = false;
+                enable-all = true;
+                opacity = 190;
+                blacklist = ["firefox" "zen-alpha"];
             };
             "org/gnome/shell/extensions/blur-my-shell/overview".style-components = 2; # dark background in overview
         };
