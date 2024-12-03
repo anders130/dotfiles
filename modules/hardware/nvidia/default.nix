eafeeccddf7e7349f4da5456e0a2b9f230839690
anders130 <93037023+anders130@users.noreply.github.com>
2024-12-03 11:47:29 +0100

    updated nvidia config

--- modules/hardware/nvidia/default.nix
+++ modules/hardware/nvidia/default.nix
@@ -1,30 +1,19 @@
-{
-    config,
-    pkgs,
-    ...
-}: {
+{pkgs, ...}: {
     services.xserver.videoDrivers = ["nvidia"];
 
-    hardware.nvidia = {
-        modesetting.enable = true;
-        powerManagement.enable = false;
-        open = false;
-        nvidiaSettings = true;
-        package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-            version = "555.42.02";
-            sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
-            sha256_aarch64 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
-            openSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
-            settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
-            persistencedSha256 = "sha256-3ae31/egyMKpqtGEqgtikWcwMwfcqMv2K4MVFa70Bqs=";
+    hardware = {
+        nvidia = {
+            modesetting.enable = true;
+            powerManagement.enable = false;
+            open = false;
+            nvidiaSettings = true;
+            nvidiaPersistenced = true;
         };
-        nvidiaPersistenced = true;
-    };
 
-    hardware.opengl = {
-        enable = true;
-        driSupport = true;
-        driSupport32Bit = true;
+        graphics = {
+            enable = true;
+            enable32Bit = true;
+        };
     };
 
     environment.systemPackages = [
