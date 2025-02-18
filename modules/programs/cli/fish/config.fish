43a4897bf6fe3341ab0092a32204b4ad666563cf
anders130 <93037023+anders130@users.noreply.github.com>
2025-02-18 16:17:12 +0100

    refactor fish module

--- modules/programs/cli/fish/config.fish
+++ modules/programs/cli/fish/config.fish
@@ -1,71 +1,65 @@
-fish_config theme choose "fish"
+set -U fish_greeting ""
+# Commands to run in interactive sessions can go here
+fish_vi_key_bindings
+set -g fish_vi_force_cursor
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
 
-if status is-interactive
-    set -U fish_greeting ""
-    # Commands to run in interactive sessions can go here
-    fish_vi_key_bindings
-    set -g fish_vi_force_cursor
-    set fish_cursor_default block
-    set fish_cursor_insert line
-    set fish_cursor_replace_one underscore
+# ctrl+p to go to previous command
+bind \cp up-or-search
+bind -M insert \cp up-or-search
 
-    # ctrl+p to go to previous command
-    bind \cp up-or-search
-    bind -M insert \cp up-or-search
+# ctrl+n to go to next command
+bind \cn down-or-search
+bind -M insert \cn down-or-search
 
-    # ctrl+n to go to next command
-    bind \cn down-or-search
-    bind -M insert \cn down-or-search
+# alias nix commands to nom
+function nix
+    set -l unfree 0
+    set -l args
 
-    # alias nix commands to nom
-    function nix
-        set -l unfree 0
-        set -l args
-
-        for arg in $argv
-            if test "$arg" = "--unfree"
-                set unfree 1
-            else
-                set args $args $arg
-            end
-        end
-
-        if test $unfree -eq 1
-            env NIXPKGS_ALLOW_UNFREE=1 nix $args --impure
+    for arg in $argv
+        if test "$arg" = "--unfree"
+            set unfree 1
         else
-            if begin test "$args[1]" = "build";
-                    or test "$args[1]" = "shell";
-                    or test "$args[1]" = "develop";
-                end
-                nom $args
-            else if test "$args[1]" = "search"
-                nh $args
-            else
-                command nix $args
+            set args $args $arg
+        end
+    end
+
+    if test $unfree -eq 1
+        env NIXPKGS_ALLOW_UNFREE=1 nix $args --impure
+    else
+        if begin test "$args[1]" = "build";
+                or test "$args[1]" = "shell";
+                or test "$args[1]" = "develop";
             end
+            nom $args
+        else if test "$args[1]" = "search"
+            nh $args
+        else
+            command nix $args
         end
     end
-
-    function ls
-        if test -f .hidden
-            set hidden_args (cat .hidden | xargs -I{} echo --ignore-glob={})
-        end
-
-        # only hide hidden files if the current command is ls
-        switch (basename (status current-command))
-            case 'ls'
-                command lsd $hidden_args $argv
-            case '*'
-                command lsd $argv
-        end
-    end
-
-    set -gx EDITOR nvim
-    set -x DIRENV_LOG_FORMAT ""
-
-    # colorize man pages with bat
-    set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
-    set -x MANROFFOPT "-c"
-
-    zoxide init fish | source
 end
+
+function ls
+    if test -f .hidden
+        set hidden_args (cat .hidden | xargs -I{} echo --ignore-glob={})
+    end
+
+    # only hide hidden files if the current command is ls
+    switch (basename (status current-command))
+        case 'ls'
+            command lsd $hidden_args $argv
+        case '*'
+            command lsd $argv
+    end
+end
+
+set -gx EDITOR nvim
+set -x DIRENV_LOG_FORMAT ""
+
+# colorize man pages with bat
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -x MANROFFOPT "-c"
--- modules/programs/cli/fish/default.nix
+++ modules/programs/cli/fish/default.nix
@@ -4,58 +4,57 @@
     username,
     ...
 }: let
-    dependencies = with pkgs; [
-        bat # better cat
-        lsd # better ls
-        nix-output-monitor # prettier nix command outputs
-        yt-dlp # youtube downloader
-        zoxide # better cd
-    ];
-
-    fishPlugins = with pkgs.fishPlugins; [
-        autopair
-        z # autosuggestions
-    ];
+    inherit (lib) mkSymlink;
 in {
     modules.programs.cli = {
         fastfetch.enable = true;
         starship.enable = true;
     };
 
+    environment = {
+        shells = [pkgs.fish];
+        systemPackages = with pkgs; [
+            lsd # better ls
+            nix-output-monitor # prettier nix command outputs
+            yt-dlp # youtube downloader
+            zoxide # better cd
+        ];
+    };
     programs.fish = {
         enable = true;
-        package = pkgs.fish;
+        interactiveShellInit = /*fish*/''
+            fish_config theme choose "fish"
+            source $HOME/.config/fish/extraConfig.fish
+            fastfetch -c $HOME/.config/fastfetch/short.jsonc
+        '';
         shellAliases = {
             ls = "lsd";
             cat = "bat";
         };
-        shellInit = /*fish*/''
-            if status is-interactive
-                fastfetch -c $HOME/.config/fastfetch/short.jsonc
-            end
-        '';
-        interactiveShellInit = /*fish*/''
-            source $HOME/.config/fish/extraConfig.fish
-        '';
     };
-
     stylix.targets.fish.enable = false;
-
-    environment.shells = [pkgs.fish];
-
     users.users.${username}.shell = pkgs.fish;
-
-    hm = {
-        home.sessionVariables.SHELL = "etc/profiles/per-user/${username}/bin/fish";
-
-        programs.bat.enable = true;
-
+    hm = { home.sessionVariables.SHELL = "etc/profiles/per-user/${username}/bin/fish";
+        programs = {
+            bat.enable = true;
+            fish = {
+                enable = true;
+                plugins = with pkgs.fishPlugins; [
+                    {
+                        name = "autopair";
+                        inherit (autopair) src;
+                    }
+                    {
+                        name = "zoxide";
+                        inherit (z) src;
+                    }
+                ];
+            };
+        };
         xdg.configFile = {
-            "fish/extraConfig.fish" = lib.mkSymlink ./config.fish;
-            "fish/functions" = lib.mkSymlink ./functions;
-            "fish/themes/fish.theme" = lib.mkSymlink ./fish.theme;
+            "fish/extraConfig.fish" = mkSymlink ./config.fish;
+            "fish/functions" = mkSymlink ./functions;
+            "fish/themes/fish.theme" = mkSymlink ./fish.theme;
         };
     };
-
-    environment.systemPackages = dependencies ++ fishPlugins;
 }
