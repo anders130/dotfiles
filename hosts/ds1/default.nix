131d3c819a9ec014d87c1f4696257ed4256de180
anders130 <93037023+anders130@users.noreply.github.com>
2025-04-02 00:24:46 +0200

    refactor and remove paperless

--- hosts/ds1/default.nix
+++ hosts/ds1/default.nix
@@ -1,15 +1,23 @@
 {
     modules = {
         bundles.server.enable = true;
-
         services = {
             caddy.enable = true; # important for paperless and vaultwarden
-            paperless.enable = true;
             vaultwarden.enable = true;
         };
     };
 
-    networking.domain = "gollub.dev";
+    networking = {
+        domain = "gollub.dev";
+        # mini network between nas and ds1
+        interfaces.enp7s0 = {
+            useDHCP = false;
+            ipv4.addresses = [{
+                address = "192.168.2.1";
+                prefixLength = 24;
+            }];
+        };
+    };
 
     boot.loader = {
         efi.canTouchEfiVariables = true;
@@ -18,13 +26,4 @@
 
     system.stateVersion = "24.11";
     hm.home.stateVersion = "24.11";
-
-    # mini network between nas and ds1
-    networking.interfaces.enp7s0 = {
-        useDHCP = false;
-        ipv4.addresses = [{
-            address = "192.168.2.1";
-            prefixLength = 24;
-        }];
-    };
 }
