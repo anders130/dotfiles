616c3781cb45bd4619aff9cbd872b47ab10c4c5b
anders130 <93037023+anders130@users.noreply.github.com>
2025-04-07 09:30:41 +0200

    refactor lumehub module

--- hosts/nix-pi/default.nix
+++ hosts/nix-pi/default.nix
@@ -1,20 +1,20 @@
 {
-    modules.bundles.rpi.enable = true;
+    modules = {
+        bundles.rpi.enable = true;
+        services.lumehub = {
+            enable = true;
+            settings.LedControllerSettings = {
+                PixelCount = 256;
+                ClockFrequency = 1500000;
+            };
+        };
+    };
 
     raspberry-pi-nix.board = "bcm2711";
 
     hardware.raspberry-pi.config.all.base-dt-params.spi = {
         enable = true;
         value = "on";
-    };
-
-    services.lumehub = {
-        enable = true;
-        openFirewall = true;
-        settings.LedControllerSettings = {
-            PixelCount = 256;
-            ClockFrequency = 1500000;
-        };
     };
 
     system.stateVersion = "24.05";
--- modules/services/lumehub/default.nix
+++ modules/services/lumehub/default.nix
@@ -1,26 +1,35 @@
 {
     config,
     inputs,
+    lib,
     ...
 }: {
     imports = [inputs.lumehub.nixosModules.default];
 
-    services.lumehub = {
-        enable = true;
-        openFirewall = true;
-        settings.LedControllerSettings.PixelCount = 200;
+    options.settings = lib.mkOption {
+        type = lib.types.attrs;
+        default = {};
+        description = "Settings for LumeHub (services.lumehub.settings)";
     };
 
-    sops.secrets.lumehub_api_key.sopsFile = ./secrets.yaml;
+    config = cfg: {
+        services.lumehub = {
+            enable = true;
+            openFirewall = true;
+            inherit (cfg) settings;
+        };
 
-    sops.templates."lumehub-secrets.json" = {
-        path = "/var/lib/lumehub/appsettings.Production.json";
-        content = /*json*/''
-        {
-            "ApiKeySettings": {
-                "ApiKey": "${config.sops.placeholder.lumehub_api_key}"
+        sops.secrets.lumehub_api_key.sopsFile = ./secrets.yaml;
+
+        sops.templates."lumehub-secrets.json" = {
+            path = "/var/lib/lumehub/appsettings.Production.json";
+            content = /*json*/''
+            {
+                "ApiKeySettings": {
+                    "ApiKey": "${config.sops.placeholder.lumehub_api_key}"
+                }
             }
-        }
-        '';
+            '';
+        };
     };
 }
