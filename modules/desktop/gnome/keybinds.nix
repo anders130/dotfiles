3e995a940a7c7ec7f1208c1c81b4b7a97ad39805
anders130 <93037023+anders130@users.noreply.github.com>
2025-04-07 09:18:19 +0200

    simplify

--- modules/desktop/gnome/keybinds.nix
+++ modules/desktop/gnome/keybinds.nix
@@ -3,40 +3,61 @@
     lib,
     ...
 }: let
+    inherit (builtins) genList length elemAt listToAttrs;
     mediaKeysPath = "org/gnome/settings-daemon/plugins/media-keys";
+    num-workspaces = 9;
 
-    mkCustomKeybindings = keybinds:
-        builtins.genList
-        (i: "/${mediaKeysPath}/custom-keybindings/custom${builtins.toString i}/")
-        (builtins.length keybinds);
+    mkIndexedAttrs = count: {nameFn, valueFn}:
+        listToAttrs (genList (i: {
+                name = nameFn i;
+                value = valueFn i;
+            })
+            count);
 
-    mkDconfSettings = keybinds:
-        builtins.listToAttrs (builtins.genList (i: {
-            name = "${mediaKeysPath}/custom-keybindings/custom${toString i}";
-            value = builtins.elemAt keybinds i;
-        }) (builtins.length keybinds));
+    mkCustomKeybindingPaths = keybinds:
+        genList (i: "/${mediaKeysPath}/custom-keybindings/custom${toString i}/")
+        (length keybinds);
 
-    keybinds = [
+    mkCustomKeybindingEntries = keybinds:
+        mkIndexedAttrs (length keybinds) {
+            nameFn = i: "${mediaKeysPath}/custom-keybindings/custom${toString i}";
+            valueFn = i: elemAt keybinds i;
+        };
+
+    mkBindings = prefix: modifier:
+        mkIndexedAttrs num-workspaces {
+            nameFn = i: "${prefix}-${toString (i + 1)}";
+            valueFn = i: ["${modifier}${toString (i + 1)}"];
+        };
+
+    mkEmptyBindings = prefix: mkIndexedAttrs num-workspaces {
+        nameFn = i: "${prefix}-${toString (i + 1)}";
+        valueFn = _: [];
+    };
+
+    keybinds = let
+        inherit (config.modules.desktop) defaultPrograms;
+    in [
         {
             name = "open-terminal";
             binding = "<Super>Return";
-            command = config.modules.desktop.defaultPrograms.terminal;
+            command = defaultPrograms.terminal;
         }
         {
             name = "open-file-manager";
             binding = "<Super>e";
-            command = config.modules.desktop.defaultPrograms.fileManager;
+            command = defaultPrograms.fileManager;
         }
         {
             name = "open-browser";
             binding = "<Super>b";
-            command = config.modules.desktop.defaultPrograms.browser;
+            command = defaultPrograms.browser;
         }
     ];
 in {
-    hm.dconf.settings = ((mkDconfSettings keybinds) // {
+    hm.dconf.settings = ((mkCustomKeybindingEntries keybinds) // {
         "${mediaKeysPath}" = {
-            custom-keybindings = mkCustomKeybindings keybinds;
+            custom-keybindings = mkCustomKeybindingPaths keybinds;
             screensaver = ["<Super>BackSpace"];
         };
 
@@ -46,49 +67,25 @@ in {
             repeat-interval = lib.hm.gvariant.mkUint32 25;
         };
 
-        "org/gnome/desktop/wm/keybindings" = {
-            close = ["<Super>c"];
-            toggle-fullscreen = ["<Super>f"];
-            toggle-maximized = ["<Super>v"];
-            move-to-workspace-1 = ["<Super><Shift>1"];
-            move-to-workspace-2 = ["<Super><Shift>2"];
-            move-to-workspace-3 = ["<Super><Shift>3"];
-            move-to-workspace-4 = ["<Super><Shift>4"];
-            move-to-workspace-5 = ["<Super><Shift>5"];
-            move-to-workspace-6 = ["<Super><Shift>6"];
-            move-to-workspace-7 = ["<Super><Shift>7"];
-            move-to-workspace-8 = ["<Super><Shift>8"];
-            move-to-workspace-9 = ["<Super><Shift>9"];
+        "org/gnome/desktop/wm/keybindings" =
+            mkBindings "switch-to-workspace" "<Super>"
+            // mkBindings "move-to-workspace" "<Super><Shift>"
+            // {
+                close = ["<Super>c"];
+                toggle-fullscreen = ["<Super>f"];
+                toggle-maximized = ["<Super>v"];
+                minimize = []; # unset Super+h
+            };
 
-            switch-to-workspace-1 = ["<Super>1"];
-            switch-to-workspace-2 = ["<Super>2"];
-            switch-to-workspace-3 = ["<Super>3"];
-            switch-to-workspace-4 = ["<Super>4"];
-            switch-to-workspace-5 = ["<Super>5"];
-            switch-to-workspace-6 = ["<Super>6"];
-            switch-to-workspace-7 = ["<Super>7"];
-            switch-to-workspace-8 = ["<Super>8"];
-            switch-to-workspace-9 = ["<Super>9"];
-
-            minimize = []; # unset Super+h
-        };
-
-        "org/gnome/shell/keybindings" = {
-            switch-to-application-1 = [];
-            switch-to-application-2 = [];
-            switch-to-application-3 = [];
-            switch-to-application-4 = [];
-            switch-to-application-5 = [];
-            switch-to-application-6 = [];
-            switch-to-application-7 = [];
-            switch-to-application-8 = [];
-            switch-to-application-9 = [];
-            toggle-message-tray = []; # this interfered with Super+v
-        };
+        "org/gnome/shell/keybindings" =
+            mkEmptyBindings "switch-to-application"
+            // {
+                toggle-message-tray = []; # interferes with Super+v
+            };
 
         "org/gnome/desktop/wm/preferences" = {
+            inherit num-workspaces;
             mouse-button-modifier = "<Super>";
-            num-workspaces = 9;
             resize-with-right-button = true;
             focus-mode = "mouse";
         };
