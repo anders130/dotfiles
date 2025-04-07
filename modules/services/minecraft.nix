b17fee55e1fae99b46fccf16eec9da6f319fae5e
anders130 <93037023+anders130@users.noreply.github.com>
2025-04-07 17:04:19 +0200

    update minecraft server config

--- modules/services/minecraft.nix
+++ modules/services/minecraft.nix
@@ -2,7 +2,9 @@
     inputs,
     pkgs,
     ...
-}: {
+}: let
+    port = 25566;
+in {
     imports = [inputs.nix-minecraft.nixosModules.minecraft-servers];
 
     services.minecraft-servers = {
@@ -13,9 +15,11 @@
 
         servers.vanilla-1-20-4 = {
             enable = true;
+            autoStart = false;
             package = pkgs.minecraftServers.vanilla-1_20_4;
+            serverProperties.server-port = port;
         };
     };
 
-    networking.firewall.allowedTCPPorts = [25565];
+    networking.firewall.allowedTCPPorts = [port];
 }
