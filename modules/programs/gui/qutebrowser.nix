11d5fa16706a011b6bcbc0fd59ef700996663160
anders130 <93037023+anders130@users.noreply.github.com>
2025-05-06 20:20:00 +0200

    modify qutebrowser settings
    make qutebrowser opaque

--- modules/programs/gui/qutebrowser.nix
+++ modules/programs/gui/qutebrowser.nix
@@ -5,7 +5,15 @@
         extraConfig = ''
             import catppuccin
             catppuccin.setup(c, 'macchiato', True)
+
+            # hide stuff
+            config.bind('xs', 'config-cycle statusbar.show always never')
+            config.bind('xt', 'config-cycle tabs.show always never')
+            config.bind('xx', 'config-cycle tabs.show always never;; config-cycle statusbar.show always never')
         '';
+        settings = {
+            tabs.position = "left";
+        };
     };
     hm.xdg.configFile."qutebrowser/catppuccin".source = inputs.catppuccin-qutebrowser;
 }
