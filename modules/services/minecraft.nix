c6b8d76d5627949950644b93551d3f865e31ad2e
anders130 <93037023+anders130@users.noreply.github.com>
2025-05-08 17:50:58 +0200

    add latest version mc server

--- modules/services/minecraft.nix
+++ modules/services/minecraft.nix
@@ -3,7 +3,7 @@
     pkgs,
     ...
 }: let
-    port = 25566;
+    server-port = 25566;
 in {
     imports = [inputs.nix-minecraft.nixosModules.minecraft-servers];
 
@@ -13,13 +13,36 @@ in {
 
         dataDir = "/srv/minecraft";
 
-        servers.vanilla-1-20-4 = {
-            enable = true;
-            autoStart = false;
-            package = pkgs.minecraftServers.vanilla-1_20_4;
-            serverProperties.server-port = port;
+        servers = let
+            defaultServer = {
+                enable = true;
+                autoStart = false;
+                serverProperties = {
+                    inherit server-port;
+                };
+            };
+        in {
+            vanilla-1-20-4 = defaultServer // {
+                package = pkgs.minecraftServers.vanilla-1_20_4;
+            };
+            vanilla-1-21-5 = defaultServer // {
+                package = pkgs.minecraftServers.vanilla-1_21_5;
+                serverProperties = defaultServer.serverProperties // {
+                    inherit server-port;
+                    difficulty = "hard";
+                    enforce-whitelist = true;
+                    white-list = true;
+                    gamemode = "survival";
+                    motd = "NixOS Minecraft Server";
+                };
+                whitelist = {
+                    "anders130" = "c2e93d01-d0d9-4e19-95e3-85bf3020b4ef";
+                    "PingPand" = "f3a1c150-11d7-453b-9f18-6173582c78ad";
+                    "CorvPauer" = "4eeadfee-16c6-40c3-ad6c-661144cea4b4";
+                };
+            };
         };
     };
 
-    networking.firewall.allowedTCPPorts = [port];
+    networking.firewall.allowedTCPPorts = [server-port];
 }
