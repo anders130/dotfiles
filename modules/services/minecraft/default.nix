daddaee553b2b31ce4c8918af47bd0a86374aeb5
anders130 <93037023+anders130@users.noreply.github.com>
2025-05-26 00:55:56 +0200

    refactor minecraft module

--- modules/services/minecraft/default.nix
+++ modules/services/minecraft/default.nix
@@ -1,9 +1,11 @@
 {
     config,
     inputs,
+    lib,
     pkgs,
     ...
 }: let
+    inherit (lib) mkMerge;
     server-port = 25566;
     rcon-port = 25575;
 in {
@@ -22,33 +24,52 @@ in {
                 autoStart = false;
                 serverProperties = {
                     inherit server-port;
+                    motd = lib.mkDefault "This is a Minecraft Server";
                 };
             };
-        in {
-            vanilla-1-20-4 = defaultServer // {
-                package = pkgs.minecraftServers.vanilla-1_20_4;
-            };
-            vanilla-1-21-5 = defaultServer // {
-                package = pkgs.minecraftServers.vanilla-1_21_5;
-                serverProperties = defaultServer.serverProperties // {
-                    inherit server-port rcon-port;
-                    difficulty = "hard";
-                    enforce-whitelist = true;
-                    white-list = true;
-                    gamemode = "survival";
-                    motd = "NixOS Minecraft Server";
-                    # RCON
+            withRcon = {
+                serverProperties = {
+                    inherit rcon-port;
                     enable-rcon = true;
                     "rcon.password" = "@RCON_PASS@";
                     server-ip = "0.0.0.0";
+                };
+            };
+            onlyFriends = {
+                serverProperties = {
+                    white-list = true;
+                    enforce-whitelist = true;
                 };
                 whitelist = {
                     "anders130" = "c2e93d01-d0d9-4e19-95e3-85bf3020b4ef";
                     "PingPand" = "f3a1c150-11d7-453b-9f18-6173582c78ad";
                     "CorvPauer" = "4eeadfee-16c6-40c3-ad6c-661144cea4b4";
                     "Timfoxi" = "edececc7-205f-4804-942b-e7122bd1fb61";
+                    "Freddyblitz" = "569ef34e-d78e-467e-b9af-c16ec4fa40bc";
+                    "lego6cat" = "81948a2b-7475-4e77-ac14-de8d2c5a249a";
+                    "Kotbaer" = "bc0a249e-b7d5-4fce-be3a-e59b889a7d99";
                 };
             };
+        in {
+            vanilla-1-20-4 = mkMerge [
+                defaultServer
+                onlyFriends
+                {
+                    package = pkgs.minecraftServers.vanilla-1_20_4;
+                }
+            ];
+            vanilla-1-21-5 = mkMerge [
+                defaultServer
+                onlyFriends
+                withRcon
+                {
+                    package = pkgs.minecraftServers.vanilla-1_21_5;
+                    serverProperties = {
+                        gamemode = "survival";
+                        motd = "NixOS Minecraft Server";
+                    };
+                }
+            ];
         };
     };
 
