83ad141c00a57823b5f0ab7378d577c10f8c078e
anders130 <93037023+anders130@users.noreply.github.com>
2025-05-26 13:30:02 +0200

    modify cobblemon modlist

--- modules/services/minecraft/default.nix
+++ modules/services/minecraft/default.nix
@@ -5,6 +5,7 @@
     pkgs,
     ...
 }: let
+    inherit (builtins) attrNames listToAttrs readDir;
     inherit (lib) genAttrs mkMerge;
     server-port = 25566;
     rcon-port = 25575;
@@ -17,7 +18,6 @@ in {
 
         dataDir = "/srv/minecraft";
         environmentFile = config.sops.secrets.minecraft.path;
-
         servers = let
             defaultServer = {
                 enable = true;
@@ -80,13 +80,34 @@ in {
                         sha512 = "sha512-vVXD/2zToZImJk1SLdtedOnbaOtt6QlGP6MA+4+gncXpVK+BSma6vdnG9oh/JJKcJoTps3BGxccGg6aYqPllmQ==";
                         stripRoot = false;
                     };
+                    extraMods = {
+                        "mods/cobbleride-fabric-0.2.5+1.21.1.jar" = pkgs.fetchurl {
+                            url = "https://cdn.modrinth.com/data/vXREhDPP/versions/7FtlwZdD/cobbleride-fabric-0.2.5%2B1.21.1.jar";
+                            sha512 = "cee9735fe7724cd48284da087f297cdd3d0f2f47a54e115eb0d1d01ced7e29479450969e48abdf635f27f1776a4969b474bab376c654538abf2ce06d88e4fca2";
+                        };
+                        "mods/fightorflight-fabric-0.7.9.jar" = pkgs.fetchurl {
+                            url = "https://cdn.modrinth.com/data/cTdIg5HZ/versions/SResvls5/fightorflight-fabric-0.7.9.jar";
+                            sha512 = "e27e4ce8f0f5be567db65f463297621b2ed5a6c1975028454739fe960179125e1a31e1c42e1f4c9dfe50810f56e73e26764eac1dc4716509360bbfc59aa36b29";
+                        };
+                    };
+                    baseMods =
+                        "${modpack}/mods"
+                        |> readDir
+                        |> attrNames
+                        |> map (name: {
+                            name = "mods/${name}";
+                            value = "${modpack}/mods/${name}";
+                        })
+                        |> listToAttrs;
                 in {
                     package = pkgs.fabricServers.fabric-1_21_1;
                     jvmOpts = "-Xmx8G -Xms4G";
-                    symlinks = genAttrs [
-                        "mods"
-                        "server-icon.png"
-                    ] (name: "${modpack}/${name}");
+                    symlinks =
+                        baseMods
+                        // extraMods
+                        // genAttrs [
+                            "server-icon.png"
+                        ] (name: "${modpack}/${name}");
                 })
             ];
         };
