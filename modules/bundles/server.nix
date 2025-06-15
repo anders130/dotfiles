0d90e63825e180494d3c153b2857a2e0f5321e44
anders130 <93037023+anders130@users.noreply.github.com>
2025-06-15 20:42:02 +0200

    fix password prompts on remote rebuild and add ssh key

--- modules/bundles/server.nix
+++ modules/bundles/server.nix
@@ -1,9 +1,17 @@
-{username, ...}: {
+{
+    config,
+    username,
+    ...
+}: {
     security.pam.sshAgentAuth.enable = true;
 
     # this allows you to run `nixos-rebuild --target-host admin@this-machine` from
     # a different host. not used in this tutorial, but handy later.
     nix.settings.trusted-users = [username];
+
+    users.users.${username}.openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBfImZ8A0bd2+tCbcikQr9H2R5j7rmov/Wp5Sa8soQqi ${username}@${config.networking.hostName}"
+    ];
 
     services.openssh.enable = true;
 
@@ -22,6 +30,10 @@
                 }
                 {
                     command = "/run/current-system/sw/bin/systemd-run";
+                    options = ["NOPASSWD"];
+                }
+                {
+                    command = "/etc/profiles/per-user/admin/bin/systemd-run";
                     options = ["NOPASSWD"];
                 }
             ];
