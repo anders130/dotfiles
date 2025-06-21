6a7f46ab2373e2c06bdf5a22f7ded4af91510fc6
anders130 <93037023+anders130@users.noreply.github.com>
2025-06-21 18:52:56 +0200

    enforce wayland for electron ...

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -16,6 +16,7 @@
             withUWSM = true;
             xwayland.enable = true;
         };
+        environment.sessionVariables.NIXOS_OZONE_WL = "1";
         xdg.portal = {
             enable = true;
             xdgOpenUsePortal = true;
