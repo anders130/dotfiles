2ecb69cd1f37d9a6f5b5e54ae1ebea5358ab3fd5
anders130 <93037023+anders130@users.noreply.github.com>
2025-07-24 20:00:18 +0200

    improve nas-to-server connection by updating the nfs version and tweaking settings

--- hosts/ds1/disk-config.nix
+++ hosts/ds1/disk-config.nix
@@ -1,22 +1,28 @@
 {lib, ...}: let
     rackflixAddress = "192.168.2.2";
-    mkNfsMount = folder: {
-        type = "nfs";
-        mountConfig.Options = "noatime";
-        what = "${rackflixAddress}:${folder}";
-        where = "/mnt/rackflix/${lib.toLower folder}";
-    };
-    mkNfsAutomount = folder: {
-        wantedBy = ["multi-user.target"];
-        automountConfig.TimeoutIdleSec = "600";
-        where = "/mnt/rackflix/${lib.toLower folder}";
-    };
-    sharedFolders = ["Data" "appdata"];
 in {
-    boot.supportedFilesystems = ["nfs"];
-
-    systemd.mounts = map mkNfsMount sharedFolders;
-    systemd.automounts = map mkNfsAutomount sharedFolders;
+    fileSystems =
+        ["Data" "appdata"]
+        |> map (folder: {
+            name = "/mnt/rackflix/${lib.toLower folder}";
+            value = {
+                device = "${rackflixAddress}:${folder}";
+                fsType = "nfs";
+                options = [
+                    "nfsvers=4.1" # latest version supported by QNAP
+                    "rsize=1048576" # max supported read size for NFSv4 (1 MiB)
+                    "wsize=1048576" # max write size
+                    "noatime"
+                    "hard"
+                    "timeo=600"
+                    "retrans=2"
+                    "noauto" # prevent mounting on boot
+                    "x-systemd.automount" # lazy mounting
+                    "x-systemd.idle-timeout=600" # auto-unmount after 600 seconds of inactivity
+                ];
+            };
+        })
+        |> builtins.listToAttrs;
 
     disko.devices.disk.nixos = {
         type = "disk";
