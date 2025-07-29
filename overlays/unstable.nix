b524ba52eef394b2f91ed13fe71f28b0ade30bfe
anders130 <93037023+anders130@users.noreply.github.com>
2025-07-29 20:09:52 +0200

    switch back to hyprlock v8.2.0

--- overlays/unstable.nix
+++ overlays/unstable.nix
@@ -1,18 +1,13 @@
 inputs: final: prev: rec {
     unstable = import inputs.nixpkgs-unstable {
-        inherit (prev) system;
-        config = {
-            allowUnfree = true;
-            allowUnsupportedSystem = true;
-            permittedInsecurePackages = [];
-        };
+        inherit (prev) config system;
     };
     # permanent unstable packages
     inherit
         (unstable)
         comma
         gemini-cli
-        hyprlock
+        # hyprlock
         immich
         nextcloud-client
         ;
