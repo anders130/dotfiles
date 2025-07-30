9e56ece1be72d533fad1dbfef72eea7b13f1de32
anders130 <93037023+anders130@users.noreply.github.com>
2025-07-30 15:46:35 +0200

    move docker-desktop wsl fix into wsl host

--- modules/services/docker/default.nix
+++
@@ -1,9 +0,0 @@
-{username, ...}: {
-    virtualisation.docker = {
-        enable = true;
-        enableOnBoot = true;
-        autoPrune.enable = true;
-    };
-
-    users.users.${username}.extraGroups = ["docker"];
-}
