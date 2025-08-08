f245d5660b864986fa4681aaa1a1b6e846b733a8
anders130 <93037023+anders130@users.noreply.github.com>
2025-08-09 00:15:27 +0200

    configure caelestia settings

---
+++ modules/desktop/caelestia/config.nix
@@ -0,0 +1,22 @@
+{config, ...}: {
+    hm.xdg.configFile."caelestia/shell.json".text = builtins.toJSON {
+        general.apps.terminal = [config.modules.desktop.defaultPrograms.terminal];
+        background = {
+            enabled = true;
+            desktopClock.enabled = true;
+        };
+        bar.status = {
+            showAudio = true;
+            showKbLayout = false;
+            showNetwork = false;
+            showBluetooth = false;
+            showBattery = false;
+        };
+        launcher = {
+            vimKeybinds = true;
+            enableDangerousActions = true;
+        };
+        notifs.actionOnClick = true;
+        session.vimKeybinds = true;
+    };
+}
