52690e9f0ea91818a69118df31f09e27bfdb0ebf
anders130 <93037023+anders130@users.noreply.github.com>
2025-08-10 20:37:51 +0200

    add options to toggle caelestia networking module

--- modules/desktop/caelestia/shell-config.nix
+++ modules/desktop/caelestia/shell-config.nix
@@ -1,23 +1,30 @@
-{config, ...}: {
-    hm.xdg.configFile."caelestia/shell.json".text = builtins.toJSON {
-        general.apps.terminal = [config.modules.desktop.defaultPrograms.terminal];
-        background = {
-            enabled = true;
-            desktopClock.enabled = true;
+{
+    config,
+    lib,
+    ...
+}: {
+    options.shell.showNetwork = lib.mkEnableOption "Show network status";
+    config = cfg: {
+        hm.xdg.configFile."caelestia/shell.json".text = builtins.toJSON {
+            general.apps.terminal = [config.modules.desktop.defaultPrograms.terminal];
+            background = {
+                enabled = true;
+                desktopClock.enabled = true;
+            };
+            bar.status = {
+                inherit (cfg.shell) showNetwork;
+                showAudio = false;
+                showAudioSwitcher = true;
+                showKbLayout = false;
+                showBluetooth = config.hardware.bluetooth.enable;
+                showBattery = config.services.upower.enable;
+            };
+            launcher = {
+                vimKeybinds = true;
+                enableDangerousActions = true;
+            };
+            notifs.actionOnClick = true;
+            session.vimKeybinds = true;
         };
-        bar.status = {
-            showAudio = false;
-            showAudioSwitcher = true;
-            showKbLayout = false;
-            showNetwork = false;
-            showBluetooth = config.hardware.bluetooth.enable;
-            showBattery = false;
-        };
-        launcher = {
-            vimKeybinds = true;
-            enableDangerousActions = true;
-        };
-        notifs.actionOnClick = true;
-        session.vimKeybinds = true;
     };
 }
