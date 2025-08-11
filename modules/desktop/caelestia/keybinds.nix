0c7160c30e36e63f03d04c2a37adea806e3b2f4b
anders130 <93037023+anders130@users.noreply.github.com>
2025-08-11 07:55:01 +0200

    add keybinds for brightness and volume buttons

--- modules/desktop/caelestia/keybinds.nix
+++ modules/desktop/caelestia/keybinds.nix
@@ -5,5 +5,14 @@
             "SUPER, BACKSPACE, global, caelestia:lock" # lock screen
             "SUPER+SHIFT, S, global, caelestia:screenshot"
         ];
+        bindl = [
+            ", XF86MonBrightnessUp, global, caelestia:brightnessUp"
+            ", XF86MonBrightnessDown, global, caelestia:brightnessDown"
+        ];
+        bindle = [
+            ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+            ", XF86AudioRaiseVolume, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ 0; wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+            ", XF86AudioLowerVolume, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ 0; wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ];
     };
 }
