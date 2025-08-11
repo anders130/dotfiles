34768262976b164f70bfb01e8479300cf1d4d0c8
anders130 <93037023+anders130@users.noreply.github.com>
2025-08-11 19:08:43 +0200

    update caelestia patches

--- overlays/caelestia-cli/default.nix
+++ overlays/caelestia-cli/default.nix
@@ -1,10 +1,17 @@
 inputs: final: prev: {
     caelestia-cli = inputs.caelestia-cli.packages.${prev.system}.default.overrideAttrs (oldAttrs: {
-        patchPhase =
+        patchPhase = let
+            inherit (builtins) attrNames readDir;
+            patches =
+                ./patches
+                |> readDir
+                |> attrNames
+                |> map (n: ./patches/${n});
+        in
             oldAttrs.patchPhase
             + ''
                 echo "Applying no-auto-scheme-wallpaper.patch"
-                patch -p1 < ${./patches/no-auto-scheme-wallpaper.patch}
+                ${builtins.concatStringsSep "\n" (map (p: "patch -p1 < ${p}") patches)}
             '';
     });
 }
