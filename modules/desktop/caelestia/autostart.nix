76b5c8e9fe5afd0a1a362cb93107e2f0984b303f
anders130 <93037023+anders130@users.noreply.github.com>
2025-08-20 23:49:21 +0200

    caelestia: increase lock delay
    this is to have the background be not black on the secondary monitors

--- modules/desktop/caelestia/autostart.nix
+++ modules/desktop/caelestia/autostart.nix
@@ -8,7 +8,7 @@
         afterFirstLogin = cmd: "bash -c 'while [ ! -f /tmp/hyprland/first_lock_done ]; do sleep 0.1; done; rm -f /tmp/hyprland/first_lock_done; while true; do if output=$(caelestia shell lock isLocked); then if [ \"$output\" = \"false\" ]; then break; fi; fi; sleep 0.5; done && ${cmd}'";
     in
         [
-            (afterShellStarted "sleep 0.3 && hyprctl dispatch global caelestia:lock && mkdir -p /tmp/hyprland && touch /tmp/hyprland/first_lock_done")
+            (afterShellStarted "sleep 0.4 && hyprctl dispatch global caelestia:lock && mkdir -p /tmp/hyprland && touch /tmp/hyprland/first_lock_done")
         ]
         ++ lib.mkAutostartList afterFirstLogin config.modules.desktop.autostart;
 }
