b4016d06189acfed1204d5c645acb76899e61515
anders130 <93037023+anders130@users.noreply.github.com>
2025-09-05 18:20:15 +0200

    patch teams to open work browser

---
+++ overlays/teams.nix
@@ -0,0 +1,22 @@
+inputs: final: prev: {
+    teams-for-linux = prev.teams-for-linux.overrideAttrs (oldAttrs: let
+        xdgOpenWrapper = final.writeShellScriptBin "xdg-open" ''
+            exec firefox -P work "$@"
+        '';
+        teamsWrapper = final.writeShellScriptBin "teams-for-linux-with-browser" ''
+            PATH=${xdgOpenWrapper}/bin:$PATH exec ${prev.teams-for-linux}/bin/teams-for-linux "$@"
+        '';
+    in {
+        desktopItems = [
+            (prev.makeDesktopItem {
+                name = "teams-for-linux";
+                exec = "${teamsWrapper}/bin/teams-for-linux-with-browser %U";
+                icon = "teams-for-linux";
+                comment = oldAttrs.meta.description;
+                desktopName = "Teams";
+                startupWMClass = "Teams";
+                categories = ["Network" "InstantMessaging" "Chat"];
+            })
+        ];
+    });
+}
