1896758f3ecfbc865fca0f0e5377dd7dd9cd33ea
anders130 <93037023+anders130@users.noreply.github.com>
2025-09-17 20:22:51 +0200

    remove manually moving mouse to middle monitor
    hyprland v0.51.0 seems to have fixed this annoyance

--- modules/desktop/hyprland-common/autostart.nix
+++ modules/desktop/hyprland-common/autostart.nix
@@ -1,15 +1,6 @@
-{
-    config,
-    pkgs,
-    ...
-}: let
-    tweaks = pkgs.writeShellScriptBin "tweaks" ''
-        hyprctl dispatch movefocus r
-        hyprctl setcursor ${config.stylix.cursor.package.name} ${toString config.stylix.cursor.size}
-    '';
-in {
+{config, ...}: {
     hm.wayland.windowManager.hyprland.settings.exec-once = [
         "gnome-keyring-daemon --start --components=secrets,pkcs11,ssh"
-        "${tweaks}/bin/tweaks"
+        "hyprctl setcursor ${config.stylix.cursor.package.name} ${toString config.stylix.cursor.size}"
     ];
 }
