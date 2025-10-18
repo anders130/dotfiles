633aa36d902e33c83484700176f01764175c0f6a
anders130 <93037023+anders130@users.noreply.github.com>
2025-10-18 17:43:33 +0200

    refactor sudo rules for servers

--- modules/bundles/server.nix
+++ modules/bundles/server.nix
@@ -4,38 +4,25 @@
     ...
 }: {
     security.pam.sshAgentAuth.enable = true;
-
-    # this allows you to run `nixos-rebuild --target-host admin@this-machine` from
-    # a different host. not used in this tutorial, but handy later.
     nix.settings.trusted-users = [username];
+    services.openssh.enable = true;
 
     users.users.${username}.openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN4fLfoAIHuVXS3tzzbw8x8P1N+Ju2uj3LmqCFs5eTiy ${username}@${config.networking.hostName}"
     ];
 
-    services.openssh.enable = true;
-
     # avoid password prompts when remote rebuilding
     security.sudo.extraRules = [
         {
             users = [username];
-            commands = [
-                {
-                    command = "/run/current-system/sw/bin/env";
-                    options = ["NOPASSWD"];
-                }
-                {
-                    command = "/run/current-system/sw/bin/nix-env";
-                    options = ["NOPASSWD"];
-                }
-                {
-                    command = "/run/current-system/sw/bin/systemd-run";
-                    options = ["NOPASSWD"];
-                }
-                {
-                    command = "/etc/profiles/per-user/admin/bin/systemd-run";
-                    options = ["NOPASSWD"];
-                }
+            commands = map (command: {
+                inherit command;
+                options = ["NOPASSWD"];
+            }) [
+                "/run/current-system/sw/bin/env"
+                "/run/current-system/sw/bin/nix-env"
+                "/run/current-system/sw/bin/systemd-run"
+                "/etc/profiles/per-user/admin/bin/systemd-run"
             ];
         }
     ];
