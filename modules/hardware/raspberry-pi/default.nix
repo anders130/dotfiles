1b31d60161ee6b374b9a4b9bdff457ac2d9f3881
anders130 <93037023+anders130@users.noreply.github.com>
2025-10-24 10:02:42 +0200

    fix hosts not evaluating without importing nixos-raspberrypi

--- modules/hardware/raspberry-pi/default.nix
+++ modules/hardware/raspberry-pi/default.nix
@@ -1,9 +1,19 @@
-{lib, ...}: {
+{
+    config,
+    lib,
+    ...
+}: {
     options.spi.enable = lib.mkEnableOption "Enable SPI";
     config = cfg: {
-        hardware.raspberry-pi.config.all.base-dt-params.spi = lib.mkIf cfg.spi.enable {
-            enable = true;
-            value = "on";
-        };
+        hardware =
+            # this is a hack to make sure hosts don't break that don't have those options
+            if !(config.hardware ? raspberry-pi)
+            then {}
+            else {
+                raspberry-pi.config.all.base-dt-params.spi = lib.mkIf cfg.spi.enable {
+                    enable = true;
+                    value = "on";
+                };
+            };
     };
 }
