534de723df6ca55b0ff5480811837e1b8e4d43d6
anders130 <93037023+anders130@users.noreply.github.com>
2025-11-23 17:23:32 +0100

    gather all local reverse proxy domains from all hosts and create entries in pihole

--- modules/services/pihole.nix
+++ modules/services/pihole.nix
@@ -1,9 +1,11 @@
 args @ {
     config,
     inputs,
+    lib,
     ...
 }: let
-    inherit (builtins) removeAttrs;
+    inherit (builtins) concatMap filter removeAttrs;
+    inherit (lib) mapAttrsToList removePrefix;
 
     webPort = 168;
     pruneModule = path: let
@@ -22,6 +24,33 @@ in {
         pihole-ftl = {
             enable = true;
             openFirewallDNS = true;
+            settings = {
+                dns = {
+                    upstreams = ["8.8.8.8" "8.8.4.4"];
+                    hosts = let
+                        extractUrls = config:
+                            config.modules.services.caddy.virtualHosts
+                            |> mapAttrsToList (name: value: {inherit name value;})
+                            |> filter ({value, ...}: value.local)
+                            |> map (
+                                {name, ...}:
+                                    name
+                                    |> removePrefix "https://"
+                                    |> removePrefix "http://"
+                            );
+                    in
+                        inputs.self.nixosConfigurations
+                        |> mapAttrsToList (_: value: value.config)
+                        |> filter (config: config.modules.utils.networking.address != null)
+                        |> concatMap (config: let
+                            inherit (config.modules.utils.networking) address;
+                            urls = extractUrls config;
+                        in
+                            map (url: "${address} ${url}") urls);
+                    listeningMode = "LOCAL";
+                };
+                dhcp.active = false;
+            };
         };
         pihole-web = {
             enable = true;
