4ce51ca81843c6c608d3ba61580ef3c394e26a70
anders130 <93037023+anders130@users.noreply.github.com>
2026-02-13 13:36:36 +0100

    refactor ntfy notifications into reusable target/source module

--- modules/services/minecraft/notifications.nix
+++ modules/services/minecraft/notifications.nix
@@ -5,55 +5,47 @@
     ...
 }: let
     inherit (lib) mapAttrsToList mkMerge;
-
-    mkNotifyService = cfg: serverName: serverCfg: let
-        ntfyUrl = config.services.ntfy-sh.settings.base-url;
-        logFile = "${cfg.dataDir}/${serverName}/logs/latest.log";
-        messageCommand = message: "curl -u \"minecraft:$NTFY_PASS\" -d \"${message}\" ${ntfyUrl}/minecraft";
-
-        script = pkgs.writeShellApplication {
-            name = "minecraft-join-notify-${serverName}";
-            runtimeInputs = with pkgs; [
-                curl
-                coreutils
-            ];
-            text = ''
-                tail -n0 -F "${logFile}" | while IFS= read -r line; do
-                  if echo "$line" | grep -q "joined the game"; then
-                    player=$(echo "$line" | sed -E 's/.*: (.*) joined the game/\1/')
-                    ${messageCommand "$player joined the ${serverName} server"}
-                  fi
-
-                  if echo "$line" | grep -q "left the game"; then
-                    player=$(echo "$line" | sed -E 's/.*: (.*) left the game/\1/')
-                    ${messageCommand "$player left the ${serverName} server"}
-                  fi
-                done
-            '';
-        };
-
-        minecraftService = "minecraft-server-${serverName}.service";
+    mkMinecraftSource = cfg: serverName: serverCfg: let
+        name = "minecraft-${serverName}";
+        service = "minecraft-server-${serverName}.service";
     in {
-        "minecraft-notify-${serverName}" = {
-            description = "Join/leave notifications for Minecraft server ${serverName}";
-            wantedBy = [minecraftService];
-            after = [minecraftService];
-            partOf = [minecraftService];
-            serviceConfig = {
-                User = "minecraft";
-                Group = "minecraft";
-                ExecStart = lib.getExe script;
-                Restart = "on-failure";
-                EnvironmentFile = config.sops.secrets.minecraft.path;
-            };
+        "${name}" = {
+            target = "minecraft";
+            user = "minecraft";
+            group = "minecraft";
+            wantedBy = [service];
+            after = [service];
+            partOf = [service];
+            script = runtimeInputs:
+                pkgs.writeShellApplication {
+                    inherit runtimeInputs;
+                    name = "notify-${name}";
+                    text = ''
+                        logFile="${cfg.dataDir}/${serverName}/logs/latest.log"
+
+                        tail -n0 -F "$logFile" | while IFS= read -r line; do
+                            if echo "$line" | grep -q "joined the game"; then
+                                player=$(echo "$line" | sed -E 's/.*: (.*) joined the game/\1/')
+                                message "$player joined the ${serverName} server"
+                            fi
+
+                            if echo "$line" | grep -q "left the game"; then
+                                player=$(echo "$line" | sed -E 's/.*: (.*) left the game/\1/')
+                                message "$player left the ${serverName} server"
+                            fi
+                        done
+                    '';
+                };
         };
     };
 in {
     config = cfg: {
-        systemd.services = mkMerge (mapAttrsToList (mkNotifyService cfg) cfg.servers);
-        services.ntfy-sh.settings = {
-            auth-users = ["minecraft:$2b$10$S8Z4rWLrBfCdPhrJkfy/jeOOoDvHPozvf3TKnwRsRFg05kfAOFp4q:user"];
-            auth-access = ["minecraft:minecraft:wo"];
+        modules.services.ntfy = {
+            targets.minecraft = {
+                secretFile = config.sops.secrets.minecraft.path;
+                authHash = "$2b$10$S8Z4rWLrBfCdPhrJkfy/jeOOoDvHPozvf3TKnwRsRFg05kfAOFp4q";
+            };
+            sources = mkMerge (mapAttrsToList (mkMinecraftSource cfg) cfg.servers);
         };
     };
 }
--- modules/services/ntfy.nix
+++ modules/services/ntfy.nix
@@ -1,31 +1,116 @@
-{config, ...}: let
+{
+    config,
+    lib,
+    pkgs,
+    ...
+}: let
+    inherit (builtins) attrNames;
+    inherit (lib) mapAttrsToList mkMerge mkOption types;
+
     domain = "ntfy.${config.networking.domain}";
     port = 2586;
+
+    ntfyUrl = config.services.ntfy-sh.settings.base-url;
+    message = pkgs.writeShellScriptBin "message" ''
+        curl -fsS -u "$NTFY_TOPIC:$NTFY_PASS" -d "$1" ${ntfyUrl}/$NTFY_TOPIC
+    '';
+    scriptDeps = with pkgs; [
+        curl
+        coreutils
+        message
+    ];
+    mkSystemdService = cfg: name: source: {
+        "notify-${name}" = {
+            description = "Notifications for ${name}";
+            inherit (source) wantedBy after partOf;
+            serviceConfig = {
+                User = source.user;
+                Group = source.group;
+                ExecStart = lib.getExe (source.script scriptDeps);
+                Restart = "on-failure";
+                EnvironmentFile = cfg.targets.${source.target}.secretFile;
+                Environment = "NTFY_TOPIC=${cfg.targets.${source.target}.topic}";
+            };
+        };
+    };
 in {
-    services = {
-        ntfy-sh = {
+    options = {
+        targets = mkOption {
+            type = types.attrsOf (types.submodule ({name, ...}: {
+                options = {
+                    topic = mkOption {
+                        type = types.str;
+                        default = name;
+                    };
+                    secretFile = mkOption {
+                        type = types.path;
+                        description = "Secret .env file for the app that contains the `NTFY_PASS` environment variable";
+                    };
+                    authHash = mkOption {
+                        type = types.str;
+                        description = "Hashed password for the app. Optain with `ntfy user hash`";
+                    };
+                };
+            }));
+            default = {};
+        };
+        sources = mkOption {
+            type = types.attrsOf (types.submodule ({name, ...}: {
+                options = {
+                    target = mkOption {
+                        type = types.str;
+                        description = "Name of the target to send notifications to";
+                        default = name;
+                    };
+                    wantedBy = mkOption {
+                        type = types.listOf types.str;
+                    };
+                    after = mkOption {
+                        type = types.listOf types.str;
+                    };
+                    partOf = mkOption {
+                        type = types.listOf types.str;
+                    };
+                    user = mkOption {
+                        type = types.str;
+                    };
+                    group = mkOption {
+                        type = types.str;
+                    };
+                    script = mkOption {
+                        type = types.functionTo types.package;
+                        description = "Function that takes the default dependencies as argument and returns a package";
+                    };
+                };
+            }));
+        };
+    };
+    config = cfg: {
+        services.ntfy-sh = {
             enable = true;
             settings = {
                 auth-default-access = "deny-all";
                 base-url = "https://${domain}";
                 listen-http = ":${toString port}";
                 behind-proxy = true;
-                auth-users = [
-                    "jesse:$2b$10$x9ZjMlkRBvh8u5ga5.dQ.OCs06iQhOqQEsGsHt4wv5VfuMbWDYMxK:admin"
-                ];
+                auth-users =
+                    ["jesse:$2b$10$x9ZjMlkRBvh8u5ga5.dQ.OCs06iQhOqQEsGsHt4wv5VfuMbWDYMxK:admin"]
+                    ++ (mapAttrsToList (name: value: "${name}:${value.authHash}:user") cfg.targets);
+                auth-access = map (name: "${name}:${name}:wo") (attrNames cfg.targets);
                 enable-login = true;
             };
         };
-    };
-    modules.services.caddy.virtualHosts.${domain} = {
-        inherit port;
-        extraConfig = ''
-            @httpget {
-                protocol http
-                method GET
-                path_regexp ^/([-_a-z0-9]{0,64}$|docs/|static/)
-            }
-            redir @httpget https://{host}{uri}
-        '';
+        systemd.services = mkMerge (mapAttrsToList (mkSystemdService cfg) cfg.sources);
+        modules.services.caddy.virtualHosts.${domain} = {
+            inherit port;
+            extraConfig = ''
+                @httpget {
+                    protocol http
+                    method GET
+                    path_regexp ^/([-_a-z0-9]{0,64}$|docs/|static/)
+                }
+                redir @httpget https://{host}{uri}
+            '';
+        };
     };
 }
