db8f29b871f2bc5b09b99c4ac0872199a52ad874
anders130 <93037023+anders130@users.noreply.github.com>
2026-03-03 21:51:02 +0100

    update lumehub and add tv_mode preset

--- hosts/nix-pi/default.nix
+++ hosts/nix-pi/default.nix
@@ -15,6 +15,23 @@
         settings = {
             led_controller.controller_type = "ws2801";
             led_controller.pixel_count = 256;
+            effects.presets.tv_mode = {
+                effect = "zone_interpolation";
+                params = {
+                    color1 = {
+                        r = 0;
+                        g = 0;
+                        b = 0;
+                    };
+                    color2 = {
+                        r = 255;
+                        g = 120;
+                        b = 20;
+                    };
+                    interpolation_length = 40;
+                    midpoint = 120;
+                };
+            };
         };
     };
 
