1c99cd101b6d776dfc2dadcc64d01d3bfeca63ef
anders130 <93037023+anders130@users.noreply.github.com>
2026-04-08 20:57:46 +0200

    fix: unfree error

--- overlays/terraria.nix
+++ overlays/terraria.nix
@@ -1,5 +1,10 @@
-inputs: final: prev: {
-    terraria-server = inputs.nixpkgs-unstable.legacyPackages.${prev.stdenv.hostPlatform.system}.terraria-server.overrideAttrs (old: {
+inputs: final: prev: let
+    unstable = import inputs.nixpkgs-unstable {
+        inherit (prev) config;
+        inherit (prev.stdenv.hostPlatform) system;
+    };
+in {
+    terraria-server = unstable.terraria-server.overrideAttrs (old: {
         version = "1.4.5.6";
         src = prev.fetchurl {
             url = "https://terraria.org/api/download/pc-dedicated-server/terraria-server-1456.zip";
