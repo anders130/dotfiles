762434bd6f00539245e45576eec4a0b2d81e280e
anders130 <93037023+anders130@users.noreply.github.com>
2026-06-02 18:15:50 +0200

    chore: move vesktop overlay

--- modules/programs/gui/discord.nix
+++ modules/programs/gui/discord.nix
@@ -3,6 +3,24 @@
     inputs,
     ...
 }: {
+    nixpkgs.overlays = [
+        (final: prev: {
+            vesktop = prev.vesktop.overrideAttrs (_: {
+                desktopItems = [
+                    (prev.makeDesktopItem {
+                        name = "discord";
+                        desktopName = "Discord";
+                        exec = "vesktop %U";
+                        icon = "${prev.discord}/share/icons/hicolor/256x256/apps/discord.png";
+                        startupWMClass = "vesktop";
+                        genericName = "Internet Messenger";
+                        keywords = ["discord" "vencord" "vesktop"];
+                        categories = ["Network" "InstantMessaging" "Chat"];
+                    })
+                ];
+            });
+        })
+    ];
     hm = {
         imports = [inputs.nixcord.homeModules.nixcord];
         programs.nixcord = {
