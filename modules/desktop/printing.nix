0c9c804b71f2f8accd61c3d3efd79c442294e215
anders130 <93037023+anders130@users.noreply.github.com>
2026-06-04 20:27:30 +0200

    feat(desktop): add scanner support and hplipWithPlugin

--- modules/desktop/printing.nix
+++ modules/desktop/printing.nix
@@ -1,15 +1,24 @@
 {
-    flake.modules.nixos.desktop = {pkgs, ...}: {
-        # Enable CUPS to print documents.
-        services.printing = {
-            enable = true;
-            drivers = [pkgs.hplip];
-        };
-        # Enable printer autodiscovery
+    flake.modules.nixos.desktop = {
+        config,
+        pkgs,
+        ...
+    }: {
         services.avahi = {
             enable = true;
             nssmdns4 = true;
             openFirewall = true;
         };
+        services.printing = {
+            enable = true;
+            drivers = [pkgs.hplipWithPlugin];
+        };
+        hardware.sane = {
+            enable = true;
+            extraBackends = [pkgs.hplipWithPlugin];
+            drivers.scanSnap.enable = true;
+        };
+        users.groups.scanner.members = config.users.normalUsers;
+        users.groups.lp.members = config.users.normalUsers;
     };
 }
