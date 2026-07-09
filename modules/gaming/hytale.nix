be029b7b48038ebd7e9ae7b433ddec36d709037f
anders130 <93037023+anders130@users.noreply.github.com>
2026-07-10 01:42:49 +0200

    fix(hytale): update

--- modules/gaming/hytale.nix
+++ modules/gaming/hytale.nix
@@ -1,6 +1,6 @@
 {
     flake-file.inputs.hytale-launcher.url = "github:anders130/hytale-launcher-nix";
     dots.gaming.provides.hytale.homeManager = {inputs', ...}: {
-        home.packages = [inputs'.hytale-launcher.packages.hytale-launcher];
+        home.packages = [inputs'.hytale-launcher.packages.default];
     };
 }
