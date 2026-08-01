53d9874c7e90627fc2b6017bdd38fcc572a97f64
anders130 <93037023+anders130@users.noreply.github.com>
2026-08-01 15:18:27 +0200

    chore(steam): open localNetworkGameTransfer ports

--- modules/gaming/steam/default.nix
+++ modules/gaming/steam/default.nix
@@ -3,6 +3,7 @@
         programs.steam = {
             enable = true;
             remotePlay.openFirewall = true;
+            localNetworkGameTransfers.openFirewall = true;
             gamescopeSession.enable = true;
             package = pkgs.steam.override {
                 extraPkgs = pkgs: [pkgs.attr];
