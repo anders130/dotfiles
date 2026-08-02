a81fd956d00f703d55ad00045a674fea54be9cfb
anders130 <93037023+anders130@users.noreply.github.com>
2026-08-02 14:17:44 +0200

    qt: use stylix theming instead of manual kvantum

--- modules/desktop/theming/qt.nix
+++ modules/desktop/theming/qt.nix
@@ -1,33 +1,9 @@
-{config, ...}: let
-    inherit (config.flake.lib) style;
-in {
-    den.aspects.theming.nixos.stylix.targets.qt.enable = false;
-
-    den.aspects.theming.homeManager = {pkgs, ...}: let
-        inherit (style) accent;
-        variant = style.flavour;
-
-        catppuccinKvantum = pkgs.catppuccin-kvantum.override {
-            inherit accent variant;
-        };
-
-        qtThemeName = "catppuccin-${variant}-${accent}";
-    in {
-        home.packages = [catppuccinKvantum];
-        qt = {
-            enable = true;
-            platformTheme.name = "kvantum";
-            style.name = "kvantum";
-        };
-        stylix.targets = {
-            qt.enable = false;
+{
+    den.aspects.theming = {
+        nixos.stylix.targets.qt.enable = true;
+        homeManager.stylix.targets = {
+            qt.enable = true;
             kde.enable = false;
-        };
-        xdg.configFile = {
-            "Kvantum/${qtThemeName}".source = "${catppuccinKvantum}/share/Kvantum/${qtThemeName}";
-            "Kvantum/kvantum.kvconfig".source = (pkgs.formats.ini {}).generate "kvantum.kvconfig" {
-                General.theme = qtThemeName;
-            };
         };
     };
 }
