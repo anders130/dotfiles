62f3e0afe9198703fa6686ac34dbfff3cf7f9ac3
anders130 <93037023+anders130@users.noreply.github.com>
2026-08-02 13:55:18 +0200

    rgb: trigger off usb coldplug instead of boot target

--- hosts/jesse-desktop/rgb.nix
+++ hosts/jesse-desktop/rgb.nix
@@ -8,24 +8,17 @@
         color = "FF4000"; # orange
         black = "000000";
         setColor = pkgs.writeShellScript "rgb-static" ''
-            for attempt in 1 2 3 4 5 6; do
-                if ${openrgb} --noautoconnect \
-                    --device 'ASUS TUF Radeon RX 9070 XT Gaming OC' --mode static --color ${color} \
-                    --device 'ASUS ROG STRIX B850-E GAMING WIFI' --mode static --color ${color} \
-                    --device 'Roccat Vulcan 120-Series Aimo' --mode direct --color ${black} \
-                    --device 'G502 HERO Gaming Mouse' --mode direct --color ${color}; then
-                    exit 0
-                fi
-                sleep 3
-            done
-            exit 1
+            ${openrgb} --noautoconnect --device 'ASUS TUF Radeon RX 9070 XT Gaming OC' --mode static --color ${color} || true
+            ${openrgb} --noautoconnect --device 'ASUS ROG STRIX B850-E GAMING WIFI' --mode static --color ${color} || true
+            ${openrgb} --noautoconnect --device 'Roccat Vulcan 120-Series Aimo' --mode direct --color ${black} || true
+            ${openrgb} --noautoconnect --device 'G502 HERO Gaming Mouse' --mode direct --color ${color} || true
         '';
     in {
+        services.udev.extraRules = ''
+            ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="0b05", ATTR{idProduct}=="19af", TAG+="systemd", ENV{SYSTEMD_WANTS}+="rgb-static-color.service"
+        '';
         systemd.services.rgb-static-color = {
             description = "Set motherboard + GPU RGB to a static color";
-            wantedBy = ["multi-user.target"];
-            after = ["systemd-udev-settle.service"];
-            wants = ["systemd-udev-settle.service"];
             serviceConfig = {
                 Type = "oneshot";
                 RemainAfterExit = true;
