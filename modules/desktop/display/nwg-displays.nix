111d44c6dc36763bd6b7d4f224491f6fbd31424e
anders130 <93037023+anders130@users.noreply.github.com>
2026-08-26 12:29:22 +0200

    fix(nwg-displays): migrate to lua

--- modules/desktop/display/nwg-displays.nix
+++ modules/desktop/display/nwg-displays.nix
@@ -1,9 +1,17 @@
 {dots, ...}: {
     dots.desktop.provides.nwg-displays = {
         includes = [dots.desktop.provides.hyprland];
-        homeManager = {pkgs, ...}: {
-            # TODO: lua: require("monitors")
+        homeManager = {
+            pkgs,
+            lib,
+            ...
+        }: {
             home.packages = [pkgs.nwg-displays];
+            wayland.windowManager.hyprland.extraLuaFiles."nwg-displays" = {
+                autoLoad = true;
+                content = ''pcall(require, "monitors")'';
+            };
+            wayland.windowManager.hyprland.settings.monitor = lib.mkForce [];
         };
     };
 }
